b9b00647ae913a2348a17ba285bc2801497a40d0
hfming225 <cd910612@gmail.com>
2017-10-06 17:11:49 +0800

    Classify 26 for version2

--- phonebook_opt.c
+++ phonebook_opt.c
@@ -7,11 +7,12 @@
 
 /* TODO: FILL YOUR OWN IMPLEMENTATION HERE! */
 entry **entryArray = NULL;
+entry **entryHeadArray = NULL;
 
 entry *findName(char lastName[], entry *pHead)
 {
     /* TODO: implement */
-    pHead = *(entryArray + lastName[0] - 97);
+    pHead = *(entryHeadArray + lastName[0] - 'a');
     while (pHead != NULL) {
         if (strcasecmp(lastName, pHead->lastName) == 0)
             return pHead;
@@ -25,15 +26,17 @@ entry *append(char lastName[], entry *e)
     /* TODO: implement */
     if(entryArray == NULL) {
         entryArray = (entry **) malloc(26*sizeof( entry * ));
+        entryHeadArray = (entry **) malloc(26*sizeof( entry * ));
     }
-    e = *(entryArray + lastName[0] - 97);
-    if (e == NULL) {
-        e = (entry*) malloc(sizeof(entry));
+    e = *(entryArray + lastName[0] - 'a');
+    if (*(entryHeadArray + lastName[0] - 'a') == NULL) {
+        e = (entry *) malloc(sizeof(entry));
+        *(entryHeadArray + lastName[0] - 'a') = e;
     }
     e->pNext = (entry *) malloc(sizeof(entry));
     e = e->pNext;
     strcpy(e->lastName, lastName);
     e->pNext = NULL;
-    *(entryArray + lastName[0] - 97) = e;
+    *(entryArray + lastName[0] - 'a') = e;
     return e;
 }
